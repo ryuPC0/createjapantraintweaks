7029190d2622861afdde809342e5572dae568433
ryuPC0 <236810795+ryuPC0@users.noreply.github.com>
2026-03-07 18:06:32 +0900

    IBE継承の追加

--- src/main/java/io/github/ryuPC0/cjptt/speedSign/SpeedSign.java
+++ src/main/java/io/github/ryuPC0/cjptt/speedSign/SpeedSign.java
@@ -2,14 +2,16 @@ package io.github.ryuPC0.cjptt.speedSign;
 
 import com.simibubi.create.content.trains.signal.SingleBlockEntityEdgePoint;
 import com.simibubi.create.foundation.block.IBE;
+import io.github.ryuPC0.cjptt.Cjptt;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 
-public class SpeedSign extends SingleBlockEntityEdgePoint /*implements IBE<SpeedSignBlockEntity>*/ {
+public class SpeedSign extends SingleBlockEntityEdgePoint implements IBE<SpeedSignBlockEntity> {
     //@Override
     public Class<SpeedSignBlockEntity> getBlockEntityClass() {
         return SpeedSignBlockEntity.class;
     }
-    /*@Override
+    @Override
     public BlockEntityType<? extends SpeedSignBlockEntity> getBlockEntityType() {
-        return ;
-    }*/
+        return Cjptt.SPEEDSIGN_BLOCKENTITY.get();
+    }
 }
