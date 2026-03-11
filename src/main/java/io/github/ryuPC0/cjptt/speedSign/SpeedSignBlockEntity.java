6c77f6b8ff07a36062923f6eee842a0afa2277d5
ryuPC0 <236810795+ryuPC0@users.noreply.github.com>
2026-03-11 15:35:19 +0900

    ScrollValueBehaviourを仮実装?

--- src/main/java/io/github/ryuPC0/cjptt/speedSign/SpeedSignBlockEntity.java
+++ src/main/java/io/github/ryuPC0/cjptt/speedSign/SpeedSignBlockEntity.java
@@ -1,11 +1,22 @@
 package io.github.ryuPC0.cjptt.speedSign;
 
 import java.util.List;
+import java.util.Objects;
 
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.simibubi.create.Create;
 import com.simibubi.create.content.trains.graph.EdgePointType;
+import com.simibubi.create.foundation.blockEntity.behaviour.ValueBoxTransform;
+import com.simibubi.create.foundation.blockEntity.behaviour.scrollValue.ScrollValueBehaviour;
+import dev.engine_room.flywheel.lib.transform.TransformStack;
 import io.github.ryuPC0.cjptt.Cjptt;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.ComponentContents;
+import net.minecraft.network.chat.Style;
+import net.minecraft.util.FormattedCharSequence;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
 import com.simibubi.create.api.contraption.transformable.TransformableBlockEntity;
@@ -17,10 +28,12 @@ import com.simibubi.create.api.equipment.goggles.IHaveGoggleInformation;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 
 public class SpeedSignBlockEntity extends SmartBlockEntity implements TransformableBlockEntity, IHaveGoggleInformation
 {
     public TrackTargetingBehaviour<SpeedSign> edgePoint;
+    public ScrollValueBehaviour throttle;
 
     public SpeedSignBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
@@ -29,6 +42,26 @@ public class SpeedSignBlockEntity extends SmartBlockEntity implements Transforma
     @Override
     public void addBehaviours(List<BlockEntityBehaviour> behaviours) {
         behaviours.add(edgePoint = new TrackTargetingBehaviour<>(this, Cjptt.SPEEDSIGN));
+        //*
+        throttle = new ScrollValueBehaviour(Component.nullToEmpty(null)
+        , this, new ValueBoxTransform() {
+            @Override
+            public void rotate(LevelAccessor level, BlockPos pos, BlockState state, PoseStack ms) {
+                TransformStack.of(ms)
+                        .rotateXDegrees(90);
+            }
+
+            @Override
+            public Vec3 getLocalOffset(LevelAccessor level, BlockPos pos, BlockState state) {
+                return new Vec3(0.5, 15.5 / 16d, 0.5);
+            }
+        });
+        throttle.between(5,100);
+        throttle.setValue(100);
+        throttle.withCallback((x)-> Objects.requireNonNull(edgePoint.getEdgePoint()).throttle= (double) 1 /throttle.getValue());
+        behaviours.add(throttle);
+
+         //*/
     }
     @Override
     public void transform(BlockEntity be, StructureTransform transform) {
