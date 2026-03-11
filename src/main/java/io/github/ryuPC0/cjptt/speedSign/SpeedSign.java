767b47f0333f2bc0b20c6be11170eb99ec55d90c
ryuPC0 <236810795+ryuPC0@users.noreply.github.com>
2026-03-11 12:11:55 +0900

    SpeedSignの方向の追加

--- src/main/java/io/github/ryuPC0/cjptt/speedSign/SpeedSign.java
+++ src/main/java/io/github/ryuPC0/cjptt/speedSign/SpeedSign.java
@@ -1,11 +1,19 @@
 package io.github.ryuPC0.cjptt.speedSign;
 
+import com.simibubi.create.content.trains.graph.EdgePointType;
 import com.simibubi.create.content.trains.signal.SingleBlockEntityEdgePoint;
 import com.simibubi.create.foundation.block.IBE;
 import io.github.ryuPC0.cjptt.Cjptt;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 
 public class SpeedSign extends SingleBlockEntityEdgePoint implements IBE<SpeedSignBlockEntity> {
+    boolean front = true;
+    @Override
+    public void blockEntityAdded(BlockEntity blockEntity, boolean front)
+    {
+        this.front = front;
+    }
     //@Override
     public Class<SpeedSignBlockEntity> getBlockEntityClass() {
         return SpeedSignBlockEntity.class;
