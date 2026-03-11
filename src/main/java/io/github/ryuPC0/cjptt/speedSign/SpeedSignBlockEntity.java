7a270529a30893671a30e4a095cde536a964791a
ryuPC0 <236810795+ryuPC0@users.noreply.github.com>
2026-03-11 11:32:19 +0900

    SpeedSignの紐づけができるように修正

--- src/main/java/io/github/ryuPC0/cjptt/speedSign/SpeedSignBlockEntity.java
+++ src/main/java/io/github/ryuPC0/cjptt/speedSign/SpeedSignBlockEntity.java
@@ -3,6 +3,8 @@ package io.github.ryuPC0.cjptt.speedSign;
 import java.util.List;
 
 
+import com.simibubi.create.content.trains.graph.EdgePointType;
+import io.github.ryuPC0.cjptt.Cjptt;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
@@ -14,6 +16,7 @@ import com.simibubi.create.foundation.blockEntity.behaviour.BlockEntityBehaviour
 import com.simibubi.create.api.equipment.goggles.IHaveGoggleInformation;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
 
 public class SpeedSignBlockEntity extends SmartBlockEntity implements TransformableBlockEntity, IHaveGoggleInformation
 {
@@ -25,10 +28,14 @@ public class SpeedSignBlockEntity extends SmartBlockEntity implements Transforma
 
     @Override
     public void addBehaviours(List<BlockEntityBehaviour> behaviours) {
-
+        behaviours.add(edgePoint = new TrackTargetingBehaviour<>(this, Cjptt.SPEEDSIGN));
     }
     @Override
     public void transform(BlockEntity be, StructureTransform transform) {
         edgePoint.transform(be, transform);
+    }
+    @Override
+    protected AABB createRenderBoundingBox() {
+        return new AABB(worldPosition, edgePoint.getGlobalPosition()).inflate(2);
     }
 }
