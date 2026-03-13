32674c209b95e3fdcb6d44b6ed9ef335642acd89
ryuPC0 <236810795+ryuPC0@users.noreply.github.com>
2026-03-13 18:34:07 +0900

    filterを追加

--- src/main/java/io/github/ryuPC0/cjptt/speedSign/SpeedSign.java
+++ src/main/java/io/github/ryuPC0/cjptt/speedSign/SpeedSign.java
@@ -1,22 +1,34 @@
 package io.github.ryuPC0.cjptt.speedSign;
 
+import com.simibubi.create.content.logistics.filter.FilterItemStack;
 import com.simibubi.create.content.trains.graph.DimensionPalette;
 import com.simibubi.create.content.trains.graph.EdgePointType;
 import com.simibubi.create.content.trains.signal.SingleBlockEntityEdgePoint;
 import com.simibubi.create.foundation.block.IBE;
+import com.simibubi.create.foundation.blockEntity.behaviour.BlockEntityBehaviour;
+import com.simibubi.create.foundation.blockEntity.behaviour.filtering.FilteringBehaviour;
 import io.github.ryuPC0.cjptt.Cjptt;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 
 public class SpeedSign extends SingleBlockEntityEdgePoint implements IBE<SpeedSignBlockEntity> {
     boolean front = true;
     double throttle = 1;
+    private FilterItemStack filter;
     @Override
     public void blockEntityAdded(BlockEntity blockEntity, boolean front)
     {
         super.blockEntityAdded(blockEntity,front);
         this.front = front;
+        FilteringBehaviour filteringBehaviour = BlockEntityBehaviour.get(blockEntity, FilteringBehaviour.TYPE);
+        if (filteringBehaviour != null)
+            setFilter(blockEntity.getLevel(), filteringBehaviour.getFilter());
+    }
+    public void setFilter(Level level, ItemStack filter) {
+        this.filter = FilterItemStack.of(filter.copy());
     }
     //@Override
     public Class<SpeedSignBlockEntity> getBlockEntityClass() {
@@ -25,6 +37,9 @@ public class SpeedSign extends SingleBlockEntityEdgePoint implements IBE<SpeedSi
     @Override
     public BlockEntityType<? extends SpeedSignBlockEntity> getBlockEntityType() {
         return Cjptt.SPEEDSIGN_BLOCKENTITY.get();
+    }
+    public FilterItemStack getFilter() {
+        return filter;
     }
     public double Getthrottle()
     {
@@ -35,11 +50,13 @@ public class SpeedSign extends SingleBlockEntityEdgePoint implements IBE<SpeedSi
     {
         super.read(nbt, migration, dimensions);
         throttle = nbt.getDouble("throttle");
+        filter = FilterItemStack.of(nbt.getCompound("Filter"));
     }
     @Override
     public void write(CompoundTag nbt, DimensionPalette dimensions)
     {
         super.write(nbt,dimensions);
         nbt.putDouble("throttle",throttle);
+        nbt.put("Filter", filter.serializeNBT());
     }
 }
--- src/main/java/io/github/ryuPC0/cjptt/speedSign/SpeedSignBlockEntity.java
+++ src/main/java/io/github/ryuPC0/cjptt/speedSign/SpeedSignBlockEntity.java
@@ -8,6 +8,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.simibubi.create.Create;
 import com.simibubi.create.content.trains.graph.EdgePointType;
 import com.simibubi.create.foundation.blockEntity.behaviour.ValueBoxTransform;
+import com.simibubi.create.foundation.blockEntity.behaviour.filtering.FilteringBehaviour;
 import com.simibubi.create.foundation.blockEntity.behaviour.scrollValue.ScrollValueBehaviour;
 import dev.engine_room.flywheel.lib.transform.TransformStack;
 import io.github.ryuPC0.cjptt.Cjptt;
@@ -16,6 +17,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.ComponentContents;
 import net.minecraft.network.chat.Style;
 import net.minecraft.util.FormattedCharSequence;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
@@ -30,11 +32,13 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 
+import javax.annotation.Nullable;
+
 public class SpeedSignBlockEntity extends SmartBlockEntity implements TransformableBlockEntity, IHaveGoggleInformation
 {
     public TrackTargetingBehaviour<SpeedSign> edgePoint;
     public ScrollValueBehaviour throttle;
-
+    private FilteringBehaviour filtering;
     public SpeedSignBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
     }
@@ -42,6 +46,7 @@ public class SpeedSignBlockEntity extends SmartBlockEntity implements Transforma
     @Override
     public void addBehaviours(List<BlockEntityBehaviour> behaviours) {
         behaviours.add(edgePoint = new TrackTargetingBehaviour<>(this, Cjptt.SPEEDSIGN));
+        behaviours.add(filtering = createFilter().withCallback(this::onFilterChanged));
         //*
         throttle = new ScrollValueBehaviour(Component.nullToEmpty(null)
         , this, new ValueBoxTransform() {
@@ -53,7 +58,7 @@ public class SpeedSignBlockEntity extends SmartBlockEntity implements Transforma
 
             @Override
             public Vec3 getLocalOffset(LevelAccessor level, BlockPos pos, BlockState state) {
-                return new Vec3(0.5, 15.5 / 16d, 0.5);
+                return new Vec3(0.5, 15.5 / 16d, 0.3);
             }
         });
         throttle.between(5,100);
@@ -62,6 +67,35 @@ public class SpeedSignBlockEntity extends SmartBlockEntity implements Transforma
         behaviours.add(throttle);
 
          //*/
+    }
+    private void onFilterChanged(ItemStack newFilter) {
+        if (level.isClientSide())
+            return;
+        SpeedSign speedSign = getSpeedSign();
+        if(speedSign != null)
+        {
+            speedSign.setFilter(level,newFilter);
+        }
+    }
+    public FilteringBehaviour createFilter() {
+        return new FilteringBehaviour(this, new ValueBoxTransform() {
+
+            @Override
+            public void rotate(LevelAccessor level, BlockPos pos, BlockState state, PoseStack ms) {
+                TransformStack.of(ms)
+                        .rotateXDegrees(90);
+            }
+
+            @Override
+            public Vec3 getLocalOffset(LevelAccessor level, BlockPos pos, BlockState state) {
+                return new Vec3(0.5, 15.5 / 16d, 0.5);
+            }
+
+        });
+    }
+    @Nullable
+    public SpeedSign getSpeedSign() {
+        return edgePoint.getEdgePoint();
     }
     @Override
     public void transform(BlockEntity be, StructureTransform transform) {
