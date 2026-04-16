8b54ec359265798d70406feffd85ba4f38c245d5
ryuPC0 <236810795+ryuPC0@users.noreply.github.com>
2026-04-16 16:47:30 +0900

    CarriageContraptionMixinを追加

---
+++ src/main/java/io/github/ryuPC0/cjptt/mixin/CarriageContraptionMixin.java
@@ -0,0 +1,28 @@
+package io.github.ryuPC0.cjptt.mixin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import com.simibubi.create.content.trains.entity.CarriageContraption;
+import io.github.ryuPC0.cjptt.Cjptt;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+import org.apache.commons.lang3.tuple.Pair;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(value = CarriageContraption.class,remap = false)
+public class CarriageContraptionMixin {
+    @Inject(method = "capture",at = @At(value = "TAIL"))
+    void Cjptt$captureExtendedControls(Level world, BlockPos pos, CallbackInfoReturnable<Pair<StructureTemplate.StructureBlockInfo, BlockEntity>> cir,@Local BlockState blockstate)
+    {
+        if(Cjptt.EXTENDED_TRAIN_CONTROLS.has(blockstate)){
+
+        }
+    }
+}
