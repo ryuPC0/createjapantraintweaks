f16b1e0862cffa8cc7e4951818dd9016acc6e089
ryuPC0 <236810795+ryuPC0@users.noreply.github.com>
2026-04-16 18:15:33 +0900

    ExtendedControlsInteractionBehaviourの継承を変更

--- src/main/java/io/github/ryuPC0/cjptt/extended/traincontrolsblock/ExtendedControlsInteractionBehaviour.java
+++ src/main/java/io/github/ryuPC0/cjptt/extended/traincontrolsblock/ExtendedControlsInteractionBehaviour.java
@@ -1,7 +1,13 @@
 package io.github.ryuPC0.cjptt.extended.traincontrolsblock;
 
+import com.simibubi.create.api.behaviour.interaction.ConductorBlockInteractionBehavior;
 import com.simibubi.create.api.behaviour.interaction.MovingInteractionBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 
-public class ExtendedControlsInteractionBehaviour extends MovingInteractionBehaviour {
+public class ExtendedControlsInteractionBehaviour extends ConductorBlockInteractionBehavior {
 
+    @Override
+    public boolean isValidConductor(BlockState blockState) {
+        return true;
+    }
 }
