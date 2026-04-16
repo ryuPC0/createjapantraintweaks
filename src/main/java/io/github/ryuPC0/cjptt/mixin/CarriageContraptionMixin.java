040863baba337d079e18df36fb93425153ab1a92
ryuPC0 <236810795+ryuPC0@users.noreply.github.com>
2026-04-16 18:16:06 +0900

    Controlsが正しく動作するよう修正

--- src/main/java/io/github/ryuPC0/cjptt/mixin/CarriageContraptionMixin.java
+++ src/main/java/io/github/ryuPC0/cjptt/mixin/CarriageContraptionMixin.java
@@ -4,12 +4,14 @@ import com.llamalad7.mixinextras.sugar.Local;
 import com.simibubi.create.content.trains.entity.CarriageContraption;
 import io.github.ryuPC0.cjptt.Cjptt;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 import org.apache.commons.lang3.tuple.Pair;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,11 +20,18 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(value = CarriageContraption.class,remap = false)
 public class CarriageContraptionMixin {
+    @Shadow private boolean sidewaysControls;
+
+    @Shadow private boolean forwardControls;
+
+    @Shadow private boolean backwardControls;
+
     @Inject(method = "capture",at = @At(value = "TAIL"))
     void Cjptt$captureExtendedControls(Level world, BlockPos pos, CallbackInfoReturnable<Pair<StructureTemplate.StructureBlockInfo, BlockEntity>> cir,@Local BlockState blockstate)
     {
         if(Cjptt.EXTENDED_TRAIN_CONTROLS.has(blockstate)){
-
+            forwardControls = true;
+            backwardControls = true;
         }
     }
 }
