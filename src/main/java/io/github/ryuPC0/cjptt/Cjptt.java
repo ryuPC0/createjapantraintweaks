5fe90fee282e1c015f2b2ffc2432d97402148c96
ryuPC0 <236810795+ryuPC0@users.noreply.github.com>
2026-04-17 15:33:21 +0900

    Logをpublicに変更

--- src/main/java/io/github/ryuPC0/cjptt/Cjptt.java
+++ src/main/java/io/github/ryuPC0/cjptt/Cjptt.java
@@ -51,7 +51,7 @@ public class Cjptt {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "cjptt";
     // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "cjptt" namespace
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
     // Create a Deferred Register to hold Items which will all be registered under the "cjptt" namespace
